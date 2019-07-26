041a9ceb6f5902eb4861f5498fae4ece9f58bc9a
Uellington Damasceno <uellington99@gmail.com>
2019-07-26 14:59:06 -0300

    Complete merge method

--- src/controllers/DatasetController.java
+++ src/controllers/DatasetController.java
@@ -1,8 +1,10 @@
 package controllers;
 
 import exceptions.CharacterNotFoundException;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.List;
 import util.Settings.DatasetId;
 import weka.core.Attribute;
 import weka.core.Instance;
@@ -28,24 +30,41 @@ public class DatasetController {
         return datasets.get(id);
     }
 
-    public Instance getHeroByName(String name) throws CharacterNotFoundException {
-        Instances dataset = getDataset(DatasetId.HEROES);
+    public Instance getHeroByName(DatasetId datasetId, int attribute, String name) throws CharacterNotFoundException {
+        Instances dataset = getDataset(datasetId);
         Enumeration<Instance> enumerateInstances = dataset.enumerateInstances();
-
         while (enumerateInstances.hasMoreElements()) {
             Instance nextElement = enumerateInstances.nextElement();
-            if (nextElement.stringValue(1).equals(name)) {
+            if (nextElement.stringValue(attribute).equals(name)) {
                 return nextElement;
             }
         }
         throw new CharacterNotFoundException();
     }
 
-    public Instances merge(Instances destinity, Instances toMerge) {
-        toMerge.stream().filter((instance) -> (destinity.contains(instance))).forEachOrdered((instance) -> {
-            destinity.add(instance);
+    public List<String[]> merge(List<String[]> datasetA, List<String[]> datasetB) {
+        ArrayList<String[]> result = new ArrayList();
+
+        String[] header = new String[((datasetA.get(0).length) + (datasetB.get(0).length - 1))];
+
+        System.arraycopy(datasetA.get(0), 0, header, 0, datasetA.get(0).length);
+        System.arraycopy(datasetB.get(0), 1, header, datasetA.get(0).length, datasetB.get(0).length - 1);
+
+        result.add(0, header);
+
+        datasetA.forEach((contentA) -> {
+            datasetB.stream().filter((contentB) -> (contentA[1].equals(contentB[0]))).map((contentB) -> {
+                int length = contentA.length + (contentB.length - 1);
+                String[] merge = new String[length];
+                System.arraycopy(contentA, 0, merge, 0, contentA.length);
+                System.arraycopy(contentB, 1, merge, contentA.length, contentB.length - 1);
+                return merge;
+            }).forEachOrdered((merge) -> {
+                result.add(merge);
+            });
         });
-        return destinity;
+
+        return result;
     }
 
     public String[] getPossibleCharacterSuggestions() {
